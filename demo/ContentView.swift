e7832a80d6100e3ea55cfa33abdb9979efab2ec8
Aeonbrk <haibinzheng10@gmail.com>
2026-01-08 22:52:22 +0800

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by oian on 2026/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by oian on 2026/1/8.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
